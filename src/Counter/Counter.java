09437ce6ad972e5b5c775bf4b5a9516318ea7d1b
fdblender <fdblender@gmail.com>
2016-06-28 15:04:53 -0400

    add second thread, mom counter

--- src/Counter/Counter.java
+++ src/Counter/Counter.java
@@ -1,21 +1,28 @@
 package Counter;
 
 public class Counter implements Runnable {
+	int sec;  // counter interval in milliseconds
+	String str;
+	
+	public Counter(int sec, String str) {
+		this.sec = sec;
+		this.str = str;
+	}
 
 @Override
 public void run() {
 	Thread ct = Thread.currentThread();
 	int count = 1;
-	System.out.println("Timer begins");
+	System.out.println(this.str + " Timer begins");
 	
 	while(true) {
 		try {
-			Thread.sleep(1000); // sleep for 1 second
+			Thread.sleep(this.sec * 1000); // sleep for 1 second
 		} catch (InterruptedException e) {
 			break;
 		}
-		System.out.println("Elapsed Time: " + count + " seconds");;
-		count++;
+		System.out.println(this.str + " Elapsed Time: " + count + " seconds");;
+		count += sec;
 	}
 	System.out.println("User paused the timer");
 
--- src/Counter/ThreadInterrupt.java
+++ src/Counter/ThreadInterrupt.java
@@ -6,14 +6,19 @@ public class ThreadInterrupt {
 
 	public static void main(String[] args) {
 		System.out.println("Press Enter Key to stop the timer");
-		Thread counter = new Thread(new Counter());
+		// create counter thread
+		Thread counter = new Thread(new Counter(4, "Counter"));
+		// create new thread to print mom every 2 seconds
+		Thread mom = new Thread(new Counter(2, "Mom"));
 		counter.start();
+		mom.start();
 		Scanner keyboard = new Scanner(System.in);
 		String s = "Start";
 		while(s.equals("Start")) {
 			s = keyboard.nextLine();
 		}		
 		counter.interrupt();
+		mom.interrupt();
 		keyboard.close();
 	}
 
